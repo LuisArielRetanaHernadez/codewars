bd7ca8f6b96bdc43b0ec473ef6ce1b9e86e98c36
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-06-15 21:06:37 -0600

    creando el codigo number-ofzero

--- 6kyu/number-of-trailing-zeros/main.js
+++ 6kyu/number-of-trailing-zeros/main.js
@@ -1,1 +1,14 @@
-const zeros = () => { }
+const zeros = (number) => {
+  let resultado = 1;
+  for (let i = 1; i <= number; i++) {
+    resultado *= i;
+  }
+  console.log(BigInt(resultado))
+  const numbers = resultado.toString().split('');
+  console.log(numbers);
+  const zeros = numbers.reverse().filter((number) => number === '0');
+  console.log(numbers);
+  return zeros;
+}
+
+console.log(zeros(30));
