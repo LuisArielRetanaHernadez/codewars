c60ee015c24a16452865e75dae37162c6c509916
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-07-23 20:24:51 -0600

    corregiendo el tiempo de ejecucion de la funcion

--- 5kyu/scramblies/index.js
+++ 5kyu/scramblies/index.js
@@ -1,11 +1,23 @@
 const scramble = (str1, str2) => {
-  const charsStr2 = str2.split('')
-  const str2Serialized = str1.split('').map((char) => {
-    if (charsStr2.includes(char)) {
-      charsStr2.splice(charsStr2.indexOf(char), 1)
-      return char
-    }
-  }).join('')
-  console.log(str2Serialized)
+  const str1Array = str1.split('')
+  const str2Array = str2.split('')
+  const str1Obj = {}
+  const str2Obj = {}
+
+  for (let i = 0; i < str1Array.length; i++) {
+    const char = str1Array[i]
+    str1Obj[char] = (str1Obj[char] || 0) + 1
+  }
+
+  for (let i = 0; i < str2Array.length; i++) {
+    const char = str2Array[i]
+    str2Obj[char] = (str2Obj[char] || 0) + 1
+  }
+
+  for (const char in str2Obj) {
+    if (!str1Obj[char] || str1Obj[char] < str2Obj[char]) return false
+  }
+
+  return true
 }
-scramble('scriptaajavx', 'javascript')
+console.log(scramble('rkoqdlw', 'world'))
