7ffe657c5a10fd988ec9eb0cc0d62dbba9a90a3a
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-08-07 17:42:15 -0600

    agregando los formatos por separa para luego acomadar el formato del tiempo a humano

--- 4kyu/Humnan-readable-duration.js
+++ 4kyu/Humnan-readable-duration.js
@@ -1,28 +1,21 @@
 const formatDuraction = seconds => {
   if (seconds <= 0) return "now"
 
-  if (seconds < 3600) {
-    const minutes = Math.floor(seconds / 60)
-    console.log(minutes)
-    const secondsReset = seconds % 60
-    return `${minutes} minute${minutes > 1 ? "s" : ""} and ${secondsReset} seconds`
-  }
+  const years = Math.floor(seconds / 31536000)
+  const days = Math.floor((seconds % 31536000) / 86400)
+  const hours = Math.floor((seconds % 86400) / 3600)
+  const minutes = Math.floor((seconds % 3600) / 60)
+  const secondsReset = seconds % 60
 
-  if (seconds >= 3600) {
-    const hours = Math.floor(seconds / 3600)
-    const minutes = Math.floor((seconds % 3600) / 60)
-    const secondsReset = seconds % 60
-    return `${hours} hour${hours > 1 ? "s" : ""}, ${minutes} minute${minutes > 1 ? "s" : ""} and ${secondsReset} seconds`
-  }
+  const formateYears = years > 0 ? `${years} year${years > 1 ? "s" : ""}` : ""
+  const formateDays = days > 0 ? `${days} day${days > 1 ? "s" : ""}` : ""
+  const formateHours = hours > 0 ? `${hours} hour${hours > 1 ? "s" : ""}` : ""
+  const formateMinutes = minutes > 0 ? `${minutes} minute${minutes > 1 ? "s" : ""}` : ""
+  const formateSeconds = secondsReset > 0 ? `${secondsReset} second${secondsReset > 1 ? "s" : ""}` : ""
 
-  if (seconds >= 2419200 && 2419200 <= seconds) {
-    const moths = Math.floor(seconds / 2419200)
-    const days = Math.floor((seconds % 2419200) / 86400)
-    const hours = Math.floor((seconds % 86400) / 3600)
-    const minutes = Math.floor((seconds % 3600) / 60)
-    const secondsReset = seconds % 60
-    return `${moths} month${moths > 1 ? "s" : ""}, ${days} day${days > 1 ? "s" : ""}, ${hours} hour${hours > 1 ? "s" : ""}, ${minutes} minute${minutes > 1 ? "s" : ""} and ${secondsReset} seconds`
-  }
+  // return [formateYears, formateMoths, formateDays, formateHours, formateMinutes, formateSeconds].filter(el => el).join(", ").replace(/,([^,]*)$/, " and$1")
+
+  return [formateYears, formateDays, formateHours, formateMinutes, formateSeconds].filter(el => el).join(", ").replace(/,(?=[^,]*$)/, " and")
 }
 
-console.log(formatDuraction(3200))
+console.log(formatDuraction(15731080))
