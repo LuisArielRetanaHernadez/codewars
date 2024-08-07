d522b8399a1dd8498cb75858ac0201d79fb06c54
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-08-07 17:07:45 -0600

    creando el formato para m, d, h, m, s

--- 4kyu/Humnan-readable-duration.js
+++ 4kyu/Humnan-readable-duration.js
@@ -14,6 +14,15 @@ const formatDuraction = seconds => {
     const secondsReset = seconds % 60
     return `${hours} hour${hours > 1 ? "s" : ""}, ${minutes} minute${minutes > 1 ? "s" : ""} and ${secondsReset} seconds`
   }
+
+  if (seconds >= 2419200 && 2419200 <= seconds) {
+    const moths = Math.floor(seconds / 2419200)
+    const days = Math.floor((seconds % 2419200) / 86400)
+    const hours = Math.floor((seconds % 86400) / 3600)
+    const minutes = Math.floor((seconds % 3600) / 60)
+    const secondsReset = seconds % 60
+    return `${moths} month${moths > 1 ? "s" : ""}, ${days} day${days > 1 ? "s" : ""}, ${hours} hour${hours > 1 ? "s" : ""}, ${minutes} minute${minutes > 1 ? "s" : ""} and ${secondsReset} seconds`
+  }
 }
 
 console.log(formatDuraction(3200))
